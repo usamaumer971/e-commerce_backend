2541773eb66e5694bf1f988eb5b377fe304d6b69
Usama Umer <59246784+usamaumer971@users.noreply.github.com>
2022-03-03 18:42:33 +0500

    url

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //url: "mongodb://localhost:27017/ecommerce"
-     url: "/usamaumer971:FutSeVtKM4HrTm9z@cluster0.zot8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+     url: "//usamaumer971:FutSeVtKM4HrTm9z@cluster0.zot8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   
 };
